f6071db1df19a2875ef37c20d8903add83f3f495
Konstantin Adamov <xrayadamo@gmail.com>
2026-08-01 19:49:01 -0700

    Updated help section with info about unsupported characters displayer as tofu box

--- src/widgets/help_dialog.rs
+++ src/widgets/help_dialog.rs
@@ -33,6 +33,7 @@ const HELP_SECTIONS: &[HelpSection] = &[
         lines: &[
             N_!("Use the arrow keys to move the selection."),
             N_!("Double-click, or press Enter, to add the selected character to the collection box."),
+            N_!("If a character is not present in the selected font, it will be shown as a tofu box."),
         ],
     },
     HelpSection {
